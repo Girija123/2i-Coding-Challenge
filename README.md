1834700107b642776afd9628fb5d01ab361c97ca
Girija Selvakumar <girijaselvakumar@Girijas-Simpplr-MacBook-Pro.local>
2023-10-10 11:49:12 +0100

    code test challenge

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 2i-Coding-Challenge
+
+## Clone using the provided link and open the code in visual studio code or any other respective IDE
+
+## In visual studio code install code runner extension 
+
+## Open codeTest.js file
+
+## Right click the file and click on run code option where the output will be displayed in output console
---
+++ codeTest.js
@@ -0,0 +1,22 @@
+/** @format */
+
+let expectedSum=15;
+
+let arrWholeNumbers=[0,15,32,2000,15000];
+
+let pairCount=0;
+
+let getSumPairCount=((expectedSum,y)=>{
+  for(let i=0;i<arrWholeNumbers.length;i++){
+      for(let j=1;j<arrWholeNumbers.length;j++){
+        if(arrWholeNumbers[i]+arrWholeNumbers[j]==expectedSum){
+           pairCount+=1;
+           arrWholeNumbers.splice(i,1);
+           arrWholeNumbers.splice(j,1);
+        }
+      }
+  }
+  return pairCount;
+});
+
+console.log(getSumPairCount(expectedSum,arrWholeNumbers));
