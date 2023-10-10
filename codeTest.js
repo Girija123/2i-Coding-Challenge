e97ee44f40d3a29461003c88f676cb22d1e88c49
Girija Selvakumar <girijaselvakumar@Girijas-Simpplr-MacBook-Pro.local>
2023-10-10 11:53:58 +0100

    amended param name

--- codeTest.js
+++ codeTest.js
@@ -6,7 +6,7 @@ let arrWholeNumbers=[0,15,32,2000,15000];
 
 let pairCount=0;
 
-let getSumPairCount=((expectedSum,y)=>{
+let getSumPairCount=((expectedSum,arrWholeNumbers)=>{
   for(let i=0;i<arrWholeNumbers.length;i++){
       for(let j=1;j<arrWholeNumbers.length;j++){
         if(arrWholeNumbers[i]+arrWholeNumbers[j]==expectedSum){
